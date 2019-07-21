07c9327bdcc90eb14168b7f90c9e850a8763e9c0
Payam Yasaie <payam.yasaie@outlook.com>
2019-07-21 08:24:19 +0430

    simplified

--- src/Crud.php
+++ src/Crud.php
@@ -12,6 +12,7 @@ use Yasaie\Paginate\Helper;
 
 class Crud
 {
+
     /**
      * @package index
      * @author  Payam Yasaie <payam@yasaie.ir>
@@ -20,11 +21,20 @@ class Crud
      * @param $heads
      * @param $sort_by
      * @param $perPage
+     * @param array $load
      *
-     * @return mixed
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public static function index($items, $heads, $sort_by, $perPage)
+    static public function index($items, $heads, $sort_by, $perPage, $load = [])
     {
+        if (!is_object($items)) {
+            $items = $items::get();
+        }
+
+        if ($load) {
+            $items = $items->load($load);
+        }
+
         # get all requests
         $request = request();
         # Url query requested
@@ -57,11 +67,26 @@ class Crud
      *
      * @param $item
      * @param $heads
+     * @param $route
+     * @param null $model
+     * @param array $load
      *
-     * @return mixed
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
      */
-    public static function show($item, $heads)
+    static public function show($item, $heads, $route = null, $model = null, $load = [])
     {
+        if (!is_object($item)) {
+            $item = $model::find($item);
+        }
+
+        if (!$item and $route) {
+            return redirect()->route($route . '.index');
+        }
+
+        if ($load) {
+            $item = $item->load($load);
+        }
+
         return view('admin.crud.show')
             ->with(compact('item', 'heads'));
     }
@@ -75,9 +100,25 @@ class Crud
      *
      * @return mixed
      */
-    public static function create($inputs, $multilang = null)
+    static public function create($inputs, $multilang = null)
     {
         return view('admin.crud.create')
             ->with(compact('inputs', 'multilang'));
+    }
+
+    /**
+     * @package destroy
+     * @author  Payam Yasaie <payam@yasaie.ir>
+     *
+     * @param $item
+     * @param null $model
+     */
+    static public function destroy($item, $model = null)
+    {
+        if (!is_object($item)) {
+            $item = $model::find($item);
+        }
+
+        $item->delete();
     }
 }
